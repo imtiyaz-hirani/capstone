95e36b963834a4a189f2070c146c3084e990b7bf
imtiyaz-hirani <i.hirani@techskillsit.com>
2023-03-21 00:13:52 +0530

    initial commit

---
+++ src/main/java/com/indecospringbackend/main/CapstoneProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.indecospringbackend.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectApiApplication.class, args);
+	}
+
+}
